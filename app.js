b97210d84770903d5aa7b2b8a3a9e1256debdc9c
RiosGustavo <gustavosuperlango@gmail.com>
2023-06-10 15:15:17 -0500

    terminatmos programa de javascript

--- app.js
+++ app.js
@@ -33,3 +33,72 @@ window.addEventListener("scroll", () => {
     header.classList.remove("active");
   }
 });
+
+/**
+ * SLIDER
+ */
+
+const slider = document.querySelector("[data-slider]");
+const sliderContainer = document.querySelector("[data-slider-container]");
+
+/** trae los elementos del archivo HTML */
+const sliderPrevBtn = document.querySelector("[data-slider-prev]");
+const sliderNextBtn = document.querySelector("[data-slider-next]");
+
+/** del archivo css se trae la viriable  "--slider-items" linea 534 */
+let totalSliderVisibleItems = Number(
+  getComputedStyle(slider).getPropertyValue("--slider-items")
+);
+
+let totalSlidableItems =
+  sliderContainer.childElementCount - totalSliderVisibleItems;
+
+let currentSlidePos = 0;
+
+const moveSliferItem = function () {
+  sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+};
+
+/**
+ * next slide
+ */
+
+const slideNext = function () {
+  const slideEnd = currentSlidePos >= totalSlidableItems;
+  if (slideEnd) {
+    currentSlidePos = 0;
+  } else {
+    currentSlidePos++;
+  }
+
+  moveSliferItem();
+};
+
+sliderNextBtn.addEventListener("click", slideNext);
+
+/**
+ * prev slide
+ */
+
+const slidePrev = function () {
+  if (currentSlidePos <= 0) {
+    currentSlidePos = totalSlidableItems;
+  } else {
+    currentSlidePos--;
+  }
+
+  moveSliferItem();
+};
+
+sliderPrevBtn.addEventListener("click", slidePrev);
+
+/**
+ * Responsive
+ */
+
+window.addEventListener("ressize", function () {
+  totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
+  totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+  moveSliderItem();
+});
