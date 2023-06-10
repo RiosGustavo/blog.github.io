9f8dc0169a56435bd381424de2a9870f0e7358df
RiosGustavo <gustavosuperlango@gmail.com>
2023-06-09 20:53:07 -0500

    agregamos imagenes y complementamos index y css

--- app.js
+++ app.js
@@ -13,6 +13,23 @@ const addEventOnElements = function (elements, eventType, callback) {
  */
 
 const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[ mobile navbar toggler]");
+const navTogglers = document.querySelectorAll("[ data-nav-toggler]");
 
-const toggleNav =
+const toggleNav = () => navbar.classList.toggle("active");
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+/**
+ * ANIMACIÓN DE CABECERA
+ * Cuando se desplace hacia abajo a 100 px, el encabezado estará activo
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
